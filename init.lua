ba0386d3335e99db4c9ad1245cdd4c6ee4f0646d
Christian "Krissy" Silvermoon <ChristianSilvermoon@users.noreply.github.com>
2022-08-08 09:33:33 -0700

    Recipe: Arrows From Iron Ingots/Nuggets
    Recipe:
     |         |  Iron   |         |
     |         |  Stick  |         |  =  4 Arrows
     |         | Feather |         |
    
     Iron can be "Ingots" or "Nuggets" based on the "mclsp_crafting_IronArrows" setting
    
    Settings:
     mclsp_crafting_IronArrows
     enum: Ingots, Nuggets, Disabled
     default value: Ingots
    
    Dependencies:
     + mcl_mobitems
     + mcl_bows

--- init.lua
+++ init.lua
@@ -2,10 +2,35 @@
 local modpath = minetest.get_modpath(minetest.get_current_modname())
 
 -- Parse Settings
-local craftingShortcuts = minetest.settings:get_bool("mclsp_crafting_Shortcuts", true)
+local craftingShortcuts  = minetest.settings:get_bool("mclsp_crafting_Shortcuts", true)
+local craftingIronArrows = minetest.settings:get("mclsp_crafting_IronArrows") or "Ingots"
 
 -- Settings Dependant Crafting Recipes
-
 if craftingShortcuts then
   dofile(modpath.."/CRAFTING/shortcuts.lua")
+end
+
+-- Crafting Arrows with Iron
+if craftingIronArrows == "Ingots" then
+
+  minetest.register_craft({
+    output = "mcl_bows:arrow 4",
+    recipe = {
+      { "", "mcl_core:iron_ingot", "" },
+      { "", "mcl_core:stick", "" },
+      { "", "mcl_mobitems:feather", "" }
+    }
+  })
+
+elseif craftingIronArrows == "Nuggets" then
+
+  minetest.register_craft({
+    output = "mcl_bows:arrow 4",
+    recipe = {
+      { "", "mcl_core:iron_nugget", "" },
+      { "", "mcl_core:stick", "" },
+      { "", "mcl_mobitems:feather", "" }
+    }
+  })
+
 end
